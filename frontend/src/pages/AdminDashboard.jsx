ac527f326e2d96ddc153f8ac51746fe9027d8cea
MalvinLH <malvinleo21@gmail.com>
2025-12-14 01:13:36 +0700

    feat: deployment trial 2

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,3 +1,4 @@
+// src/pages/AdminDashboard.jsx
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { deleteManualInsight, listInsights } from "../api/insightsApi";
