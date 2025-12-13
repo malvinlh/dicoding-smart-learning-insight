8b0a796f6ee39fcdb0a82c99c850d0ca82c150ef
MalvinLH <malvinleo21@gmail.com>
2025-12-13 23:23:39 +0700

    fix: backend deployment

--- backend/src/services/db.js
+++ backend/src/services/db.js
@@ -2,6 +2,8 @@ const { Pool } = require('pg');
 
 const connectionString = process.env.DATABASE_URL;
 
+console.log('DATABASE_URL =', process.env.DATABASE_URL);
+
 const pool = new Pool(
   connectionString
     ? {
